724bb796a5b030c32079db899d561d9595b9eeb9
Nicole C <ntc27@cornell.edu>
2017-04-09 08:30:14 -0400

    Add files via upload
    flask gives 400 bad request bug pls fix

--- HackNYSpring2017-master/app/__init__.py
+++ HackNYSpring2017-master/app/__init__.py
@@ -6,16 +6,39 @@ app.config.from_object(__name__)
 
 @app.route("/")
 def main():
-    return render_template('index.html', data="hello")
+    return render_template('index.html')
 
+@app.route("/", methods = ['POST'])
+def register():
+	name = request.form['name']
+	phone = request.form['phoneNumber']
+	zipcode = request.form['zipcode']
+	gmail = request.form['gmail']
+	password = request.form['password']
+    #store into database
+	return render_template('index.html', msg = "You successfully registered. Please log in again.")
 
 @app.route("/dashboard", methods = ['POST'])
-def my_form_post():
-    text = request.form['phone']
-    password = request.form['']
-    phoneNumber = request["phoneNumber"]
-    
-    return render_template('dashboard.html')
+def login():
+    phoneNum = request.form['phone']
+    password = request.form['pw']
+    #compare to database, if math, render dashboard, else, return to index
+	#pull to check if alarm is being set, if not, then set alarmTime to "not set"
+    return render_template('dashboard.html', alarmTime = "insert data from database")
+	
+@app.route("/dashboard", methods = ['POST'])
+def edit():
+	alarm = request.form['alarmTime']
+	news = request.form['category']
+	alarmmsg = ""
+	newsmsg = ""
+	if alarm is not None: 
+		#store alarm data into database
+		alarmmsg = "New alarm time has been set!"
+	if news is not None: 
+		#store news data into database
+		newsmsg = "News categories have been set!"
+	return render_template('dashboard.html', alarmTime = alarm, alarmMsg = alarmmsg, newsMsg = newsmsg)
 
 if __name__ == "__main__":
     app.run(debug=True)
